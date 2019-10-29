97460e61f239eb69306910b55e7cee2ad298484d
myrick11 <56942167+myrick11@users.noreply.github.com>
2019-10-28 21:16:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Myrick_Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("From Dom");
+        }
+    }
+}
